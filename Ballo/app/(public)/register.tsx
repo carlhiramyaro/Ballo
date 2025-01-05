ddf6e9e120c3a907f54d51652484d2d9ee823ba8
carlhiramyaro <carlhiramyaro@outlook.com>
2025-01-05 00:26:01 +0000

    auth working

--- Ballo/app/(public)/register.tsx
+++ Ballo/app/(public)/register.tsx
@@ -1,9 +1,140 @@
-import { View, Text } from "react-native";
+import { Button, TextInput, View, StyleSheet } from "react-native";
+import { useSignUp } from "@clerk/clerk-expo";
+import Spinner from "react-native-loading-spinner-overlay";
+import { useState } from "react";
+import { Stack } from "expo-router";
 
-export default function Register() {
+const Register = () => {
+  const { isLoaded, signUp, setActive } = useSignUp();
+
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [pendingVerification, setPendingVerification] = useState(false);
+  const [code, setCode] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  // Create the user and send the verification email
+  const onSignUpPress = async () => {
+    if (!isLoaded) {
+      return;
+    }
+    setLoading(true);
+
+    try {
+      // Create the user on Clerk with CAPTCHA disabled
+      await signUp.create({
+        emailAddress,
+        password,
+        unsafeMetadata: {
+          skipCaptcha: true,
+        },
+      });
+
+      // Send verification Email
+      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+
+      // change the UI to verify the email address
+      setPendingVerification(true);
+    } catch (err: any) {
+      alert(err.errors[0].message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Verify the email address
+  const onPressVerify = async () => {
+    if (!isLoaded) {
+      return;
+    }
+    setLoading(true);
+
+    try {
+      const completeSignUp = await signUp.attemptEmailAddressVerification({
+        code,
+      });
+
+      await setActive({ session: completeSignUp.createdSessionId });
+    } catch (err: any) {
+      alert(err.errors[0].message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Register Screen</Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{ headerBackVisible: !pendingVerification }} />
+      <Spinner visible={loading} />
+
+      {!pendingVerification && (
+        <>
+          <TextInput
+            autoCapitalize="none"
+            placeholder="simon@galaxies.dev"
+            placeholderTextColor="#999"
+            value={emailAddress}
+            onChangeText={setEmailAddress}
+            style={styles.inputField}
+          />
+          <TextInput
+            placeholder="password"
+            placeholderTextColor="#999"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+            style={styles.inputField}
+          />
+
+          <Button
+            onPress={onSignUpPress}
+            title="Sign up"
+            color={"#6c47ff"}
+          ></Button>
+        </>
+      )}
+
+      {pendingVerification && (
+        <>
+          <View>
+            <TextInput
+              value={code}
+              placeholder="Code..."
+              placeholderTextColor="#999"
+              style={styles.inputField}
+              onChangeText={setCode}
+            />
+          </View>
+          <Button
+            onPress={onPressVerify}
+            title="Verify Email"
+            color={"#6c47ff"}
+          ></Button>
+        </>
+      )}
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+  },
+  inputField: {
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 1,
+    borderColor: "#6c47ff",
+    borderRadius: 4,
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+  button: {
+    margin: 8,
+    alignItems: "center",
+  },
+});
+
+export default Register;
