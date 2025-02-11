f09e392bd64ecd637f618443a9049032fc2a667b
carlhiramyaro <carlhiramyaro@outlook.com>
2025-02-11 13:02:07 -0500

    starting maps feature

--- Ballo/app.config.js
+++ Ballo/app.config.js
@@ -1,13 +1,42 @@
 import "dotenv/config";
 
-export default {
-  expo: {
-    name: "Ballo",
-    extra: {
-      clerkPublishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
+export default ({ config }) => {
+  return {
+    ...config,
+    expo: {
+      name: "Ballo",
+      slug: "ballo",
+      version: "1.0.0",
+      orientation: "portrait",
+      icon: "./assets/images/icon.png",
+      scheme: "myapp",
+      userInterfaceStyle: "automatic",
+      newArchEnabled: true,
+      ios: {
+        bundleIdentifier: "com.carlhiramyaro.ballo",
+        supportsTablet: true,
+        buildNumber: "1.0.0",
+        infoPlist: {
+          ITSAppUsesNonExemptEncryption: false,
+        },
+      },
+      android: {
+        package: "com.carlhiramyaro.ballo",
+        adaptiveIcon: {
+          foregroundImage: "./assets/images/adaptive-icon.png",
+          backgroundColor: "#ffffff",
+        },
+      },
+      extra: {
+        ...config.extra,
+        clerkPublishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
+        eas: {
+          projectId: "a2b926c2-ff26-41e6-92e4-f299cc8c8a05",
+        },
+      },
+      web: {
+        bundler: "metro",
+      },
     },
-    web: {
-      bundler: "metro",
-    },
-  },
+  };
 };
